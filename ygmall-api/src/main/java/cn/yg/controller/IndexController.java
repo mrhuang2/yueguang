ec97222518276574110c239cfcb6a058e56788bc
zhongyue.huang <2249494390@qq.com>
2020-04-05 00:09:32 +0800

    basic commit

---
+++ ygmall-api/src/main/java/cn/yg/controller/IndexController.java
@@ -0,0 +1,17 @@
+package cn.yg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("index")
+public class IndexController {
+
+
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+
+
+}
